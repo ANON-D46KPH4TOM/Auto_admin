6f21bcc35e56948d483192350b6519a922dd1206
Mr F1d41 <105339452+F1d41@users.noreply.github.com>
2022-06-01 16:46:06 +0530

    Update admin.sh

--- admin.sh
+++ admin.sh
@@ -1,1 +1,23 @@
 clear
+echo ""
+echo ""
+echo ""
+echo ""
+cd
+if [ -d storage ]; then
+echo ""
+echo ""
+clear
+else
+termux-setup-storage -y
+echo ""
+echo ""
+echo ""
+clear
+fi
+clear
+pkg install figlet
+clear
+pkg install python -y
+clear
+bash .img/
