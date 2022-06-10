14a7fd807179323c341a03871a5799903e6e2b67
Mr F1d41 <105339452+F1d41@users.noreply.github.com>
2022-06-11 01:08:56 +0530

    Add files via upload

---
+++ admin.sh
@@ -0,0 +1,49 @@
+fpp_Community="\033[0;32m"
+if [ -d /data/data/com.termux/files/home/Auto_admin ]; then
+echo ""
+echo ""
+clear
+else
+echo ""
+cd ..
+mv Auto_admin ~
+clear
+fi
+if [ -d  /data/data/com.termux/files/home/storage ]; then
+echo ""
+echo ""
+clear
+else
+termux-setup-storage -y
+echo ""
+echo ""
+echo ""
+clear
+fi
+pkg install figlet
+clear
+pkg install python -y
+clear
+echo -e $fpp_Community
+clear
+echo " Please Wait..."
+echo " █"
+sleep 1
+echo " ██"
+echo " ████"
+sleep 1
+echo " ████████"
+echo " ████████████████"
+sleep 1
+echo -e $fpp_Community
+clear
+echo " Installation Completed"
+cd .img
+bash WhatsApp.sh
+clear
+cd
+cd Auto_admin
+cd .img
+bash WhatsApp.sh
+clear
+exit
